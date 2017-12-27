2bc6db436e9c9aa6304fca949fa06f6b9eb9394e
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-27 02:52:50 +0100

    Cretae class Basket frame

--- frompythontojava/onlineshop/part1/Basket.java
+++ frompythontojava/onlineshop/part1/Basket.java
@@ -1,7 +1,25 @@
 package frompythontojava.onlineshop.part1;
-// import frompythontojava.onlineshop.part1.Iterator;
+
+import java.util.ArrayList;
+
+import frompythontojava.onlineshop.part1.Iterator;
 
 public class Basket{
+    private ArrayList <Product> productList;
+    private Iterator iterator; //? bez new? skoro nie ma konstruktora, to nie powinno byc tu?
+
+    public Iterator getIterator(){
+        return this.iterator; //new Iterator?
+    }
+
+    public void addProduct(Product product){
+        ;
+    }
+
+    public boolean removeProduct(Product product){
+        ;
+    }
+
     private class ProductIterator implements Iterator{
         public boolean hasNext(){
             ;
