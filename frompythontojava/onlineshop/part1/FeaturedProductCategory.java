8f982759245451353d9bb1b261cf605cf2281777
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-27 11:21:03 +0100

    Create class FeaturedProductCategory frame; Add package declaration

---
+++ frompythontojava/onlineshop/part1/FeaturedProductCategory.java
@@ -0,0 +1,12 @@
+package frompythontojava.onlineshop.part1;
+
+import java.util.Date;
+import frompythontojava.onlineshop.part1.ProductCategory;
+
+public class FeaturedProductCategory extends ProductCategory{
+    private Date expirationDate;
+
+    public FeaturedProductCategory(String name, Date expirationDate){
+        ;
+    }
+}
