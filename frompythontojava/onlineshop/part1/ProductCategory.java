24e3b3bc5ad6cc045c69f4b59523984fd80ce301
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-27 11:17:22 +0100

    Create class ProductCategory frame; Add package declaration

---
+++ frompythontojava/onlineshop/part1/ProductCategory.java
@@ -0,0 +1,15 @@
+package frompythontojava.onlineshop.part1;
+
+
+public class ProductCategory{
+    private String name;
+    private Integer ID;
+
+    public ProductCategory(){
+        ;
+    }
+
+    public ProductCategory(String name){
+        ;
+    }
+}
