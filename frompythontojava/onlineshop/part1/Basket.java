3cbb7aae407a466eba126d7b5765f8690b30d972
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-27 01:45:54 +0100

    Create class Basket & inner class ProductIterator; Add package declaration

---
+++ frompythontojava/onlineshop/part1/Basket.java
@@ -0,0 +1,8 @@
+package frompythontojava.onlineshop.part1;
+// import frompythontojava.onlineshop.part1.Iterator;
+
+public class Basket{
+    private class ProductIterator implements Iterator{
+        ;
+    }
+}
