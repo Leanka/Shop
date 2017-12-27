e888932c48e7f812a7bb0b40c8f2a192f5f835cf
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-27 11:24:17 +0100

    Fill up Product class frame

---
+++ frompythontojava/onlineshop/part1/Product.java
@@ -0,0 +1,31 @@
+package frompythontojava.onlineshop.part1;
+
+import java.util.ArrayList;
+
+public class Product{
+    private String name;
+    private Float defaultPrice;
+    private ProductCategory productCategory;
+    private Integer ID;
+    private static ArrayList productList;
+
+    public Product(){
+        ;
+    }
+
+    public Product(String name, Float defaulPrice, ProductCategory productCategory){
+        ;
+    }
+
+    public String toString(){
+        ;
+    }
+
+    public ArrayList getAllProducts(){
+        ;
+    }
+
+    public ArrayList getAllProductsBy(ProductCategory productCategory){
+        ;
+    }
+}
