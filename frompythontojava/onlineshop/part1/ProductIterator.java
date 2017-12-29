235bdc5c8408d1287823e4f7f80f479d4e8a70d1
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-29 16:31:00 +0100

    Add placeholders to return statements

--- frompythontojava/onlineshop/part1/ProductIterator.java
+++ frompythontojava/onlineshop/part1/ProductIterator.java
@@ -2,9 +2,9 @@ package frompythontojava.onlineshop.part1;
 
 public class ProductIterator implements Iterator{
     public boolean hasNext(){
-        ;
+        return false;
     }
     public Object next(){
-        ;
+        return null;
     }
 }
