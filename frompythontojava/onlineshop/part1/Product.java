207dc3202be3f26265c8818e25521f85e565affe
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-30 00:47:19 +0100

    Change string format in toString(); Fill up constructor; Assign value of 1 to ID field

--- frompythontojava/onlineshop/part1/Product.java
+++ frompythontojava/onlineshop/part1/Product.java
@@ -6,7 +6,7 @@ public class Product{
     private String name;
     private Float defaultPrice;
     private ProductCategory productCategory;
-    private Integer ID;
+    private Integer ID = 1;
     private static ArrayList productList;
 
     public Product(){
@@ -14,11 +14,14 @@ public class Product{
     }
 
     public Product(String name, Float defaulPrice, ProductCategory productCategory){
-        ;
+        this.name = name;
+        this.defaultPrice = defaulPrice;
+        this.productCategory = productCategory;
+        this.ID = ID;
     }
 
     public String toString(){
-        return new String();
+        return "ID:" + this.ID + ",name:" +this.name + ",price: " + this.defaultPrice + ",category: " + this.productCategory;
     }
 
     public ArrayList getAllProducts(){
