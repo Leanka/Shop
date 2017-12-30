25935072f511184ee5e3b76a547f2578e0b870af
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-30 21:06:51 +0100

    Add idCounter field, initialize idCounter & product list; Fill constructor with no args; remove test print from toString() & fill it up; Change returned value of getAllProducts to productList field; In getProductsBy() replace placeholder with method implementation; Add getCounter()

--- frompythontojava/onlineshop/part1/Product.java
+++ frompythontojava/onlineshop/part1/Product.java
@@ -6,29 +6,54 @@ public class Product{
     private String name;
     private Float defaultPrice;
     private ProductCategory productCategory;
-    private Integer ID = 1;
-    private static ArrayList productList;
+    private Integer ID;
+    private static Integer idCounter = 0;
+    private static ArrayList <Product> productList = new ArrayList<Product>();
 
     public Product(){
-        ;
+        this.ID = idCounter++;
+
+        productList.add(this);
     }
 
     public Product(String name, Float defaulPrice, ProductCategory productCategory){
         this.name = name;
         this.defaultPrice = defaulPrice;
         this.productCategory = productCategory;
-        this.ID = ID;
+        this.ID = idCounter++;
+
+        productList.add(this);
     }
 
     public String toString(){
-        return "ID:" + this.ID + ",name:" +this.name + ",price: " + this.defaultPrice + ",category: " + this.productCategory;
+        if(this.name == null){
+            return "ID:" + this.ID + ",name:UNKNOWN PRODUCT";
+        }else{
+            return "ID:" + this.ID + 
+                    ",name:" +this.name + 
+                    ",price: " + this.defaultPrice + "," + 
+                    this.productCategory.getInfo();    
+        }
+        
     }
 
-    public ArrayList getAllProducts(){
-        return new ArrayList();
+    public ArrayList <Product> getAllProducts(){
+        return productList;
     }
 
-    public ArrayList getAllProductsBy(ProductCategory productCategory){
-        return new ArrayList ();
+    public ArrayList <Product> getAllProductsBy(ProductCategory productCategory){
+        ArrayList <Product> productsByCategory = new ArrayList<Product>();
+        for(int index = 0; index < productList.size(); ++index){
+            Product product = productList.get(index);
+            if(productCategory == product.productCategory){
+                productsByCategory.add(product);
+            }
+        }
+        return productsByCategory;
     }
+
+    public static Integer getIdCounter(){
+        return idCounter;
+    }
+
 }
