f232139e86e0ab1f2a5bb4daa7ffb78c22b692b1
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-31 15:35:39 +0100

    Fill pay() method in PaymentProcess class

--- frompythontojava/onlineshop/part2/PaymentProcess.java
+++ frompythontojava/onlineshop/part2/PaymentProcess.java
@@ -2,7 +2,11 @@ package frompythontojava.onlineshop.part2;
 
 public class PaymentProcess extends AbstractProcess{
     protected void action(Orderable item){
-        ;
+        if(item.pay()){
+            System.out.println("Status has been succesfully changed to " + ((Order)item).getStatus());
+        }else{
+            System.out.println("Invalid operation. Current status (" + ((Order)item).getStatus() + ") requiers different action.");
+        }
     }
 
     
