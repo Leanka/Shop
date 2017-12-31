b973dc040de96ab3555a7bbeb98b2581c5b742e0
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-31 15:35:22 +0100

    Fill checkout() method in CheckoutProcess class

--- frompythontojava/onlineshop/part2/CheckoutProcess.java
+++ frompythontojava/onlineshop/part2/CheckoutProcess.java
@@ -2,7 +2,11 @@ package frompythontojava.onlineshop.part2;
 
 public class CheckoutProcess extends AbstractProcess{
     protected void action(Orderable item){
-        ;
+        if(item.checkout()){
+            System.out.println("Status has been succesfully changed to " + ((Order)item).getStatus());
+        }else{
+            System.out.println("Invalid operation. Current status (" + ((Order)item).getStatus() + ") requiers different action.");
+        }
     }
     
 }
