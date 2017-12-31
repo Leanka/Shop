a39429961439bc5c0ead31b47d9c8789823ff282
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-12-31 01:17:47 +0100

    Create & fill PaymentProcess class frame; Add package declaration

---
+++ frompythontojava/onlineshop/part2/PaymentProcess.java
@@ -0,0 +1,13 @@
+package frompythontojava.onlineshop.part2;
+
+public class PaymentProcess extends AbstractProcess{
+    protected void action(Orderable item){
+        ;
+    }
+
+    @Override
+    public boolean pay(){
+        return super.pay();
+    }
+    
+}
