8f33b8eb980ba945577154853ba794be01ddea38
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-01 23:04:27 +0100

    Stock class: Refactor: Remove unnecesary empty lines, add 'this' when using instance fields

--- frompythontojava/onlineshop/part3/Stock.java
+++ frompythontojava/onlineshop/part3/Stock.java
@@ -10,8 +10,8 @@ import java.util.Date;
 
 public class Stock{
     private final Product PRODUCT = new Product();
-    private ArrayList <ProductCategory> productCategory = new ArrayList <ProductCategory>();
     private View view = new View();
+    private ArrayList <ProductCategory> productCategory = new ArrayList <ProductCategory>();
     private final int INCORRECT_INPUT = -1;
     private final ProductCategory CATEGORY = new ProductCategory();
 
@@ -21,9 +21,9 @@ public class Stock{
         ProductCategory fruits = new ProductCategory("Fruits");
         ProductCategory vegetables = new ProductCategory("Vegetable");
         
-        productCategory.add(fruits);
-        productCategory.add(vegetables);
-        productCategory.add(new ProductCategory("Baked goods"));
+        this.productCategory.add(fruits);
+        this.productCategory.add(vegetables);
+        this.productCategory.add(new ProductCategory("Baked goods"));
 
         new Product("Apple", 1.05f, fruits);
         new Product("Mango", 13.50f, fruits);
@@ -39,18 +39,18 @@ public class Stock{
     
     public void createNewProduct(){
         ProductCategory chosenCategory = getCategory();
-        if(chosenCategory == CATEGORY){
-            view.print("Adding product terminated.");
+        if(chosenCategory == this.CATEGORY){
+            this.view.print("Adding product terminated.");
             return;
         }
-        String name = view.getInput("Pass new products name: ");
+        String name = this.view.getInput("Pass new products name: ");
         
         float price;
         try{
             price = Float.valueOf(view.getInput("Pass new products price: "));
         }catch(NumberFormatException errorMessage){
-            view.print(errorMessage);
-            view.print("Adding product terminated.");
+            this.view.print(errorMessage);
+            this.view.print("Adding product terminated.");
             return;
         }
 
@@ -64,8 +64,8 @@ public class Stock{
 
     public void removeProduct(){
         Product chosenProduct = getProduct();
-        if(chosenProduct == PRODUCT){
-            view.print("Incorrect product choice. Operation terminated");
+        if(chosenProduct == this.PRODUCT){
+            this.view.print("Incorrect product choice. Operation terminated");
         }else{
             this.PRODUCT.removeProduct(chosenProduct);
         } 
@@ -77,17 +77,17 @@ public class Stock{
     }
 
     public void showAllProduct(){
-        ArrayList <Product> productList = PRODUCT.getAllProducts();
-        view.print("All products:");
+        ArrayList <Product> productList = this.PRODUCT.getAllProducts();
+        this.view.print("All products:");
         for(int index = 0; index < productList.size(); ++index){
-            view.print(productList.get(index).toString());
+            this.view.print(productList.get(index).toString());
         }
         view.print();
     }
     
     public void addNewCategory(){
         String categoryName = view.getInput("Pass category name: ");
-        productCategory.add(new ProductCategory(categoryName));
+        this.productCategory.add(new ProductCategory(categoryName));
     }
 
     public void addNewFeaturedCategory(){
@@ -99,12 +99,12 @@ public class Stock{
     }
 
     public void showAllCategories(){
-        view.print("All categories:");
-        for(int index = 0; index < productCategory.size(); ++index){
-            view.print("ID: " + productCategory.get(index).getID() + 
-                        ". " + productCategory.get(index).getInfo());
+        this.view.print("All categories:");
+        for(int index = 0; index < this.productCategory.size(); ++index){
+            this.view.print("ID: " + this.productCategory.get(index).getID() + 
+                        ". " + this.productCategory.get(index).getInfo());
         }
-        view.print();
+        this.view.print();
     }
 
     public Integer getNumberFromUser(String message){
@@ -112,8 +112,8 @@ public class Stock{
         try{
             itemId = Integer.valueOf(view.getInput(message));
         }catch(NumberFormatException errorMessage){
-            view.print(errorMessage);
-            view.print("Getting input terminated.");
+            this.view.print(errorMessage);
+            this.view.print("Getting input terminated.");
         }
         return itemId;
     }
@@ -121,8 +121,8 @@ public class Stock{
     public Product getProduct(){
         showAllProduct();
         Integer productId = getNumberFromUser("Pass Products ID: ");
-        if(productId == INCORRECT_INPUT){
-            return PRODUCT;
+        if(productId == this.INCORRECT_INPUT){
+            return this.PRODUCT;
         }
 
         ArrayList <Product> productsList = this.PRODUCT.getAllProducts();
@@ -131,33 +131,33 @@ public class Stock{
                 return productsList.get(index);
             }
         }
-        return PRODUCT;
+        return this.PRODUCT;
     }
 
     public ProductCategory getCategory(){
         showAllCategories();
         Integer categoryId = getNumberFromUser("Pass category ID: ");
-        if(categoryId == INCORRECT_INPUT){
-            return CATEGORY;
+        if(categoryId == this.INCORRECT_INPUT){
+            return this.CATEGORY;
         }
 
-        for(int index = 0; index < productCategory.size(); ++index){
-            if(categoryId.equals(productCategory.get(index).getID())){
-                return productCategory.get(index);
+        for(int index = 0; index < this.productCategory.size(); ++index){
+            if(categoryId.equals(this.productCategory.get(index).getID())){
+                return this.productCategory.get(index);
             }
         }
-        return CATEGORY;
+        return this.CATEGORY;
     }
     public void showAllProductsInCategory(){
         ProductCategory chosenCategory = getCategory();
-        if(chosenCategory == CATEGORY){
-            view.print("Adding product terminated.");
+        if(chosenCategory == this.CATEGORY){
+            this.view.print("Adding product terminated.");
             return;
         }
 
         ArrayList <Product> allProductsByChosenCategory = PRODUCT.getAllProductsBy(chosenCategory);
         if(allProductsByChosenCategory.size() == 0){
-            view.print("No products in " + chosenCategory.getName() + " category");
+            this.view.print("No products in " + chosenCategory.getName() + " category");
             return;
         }
 
@@ -168,14 +168,14 @@ public class Stock{
 
     public Product getProductByCategory(){
         ProductCategory chosenCategory = getCategory();
-        if(chosenCategory == CATEGORY){
-            return PRODUCT;
+        if(chosenCategory == this.CATEGORY){
+            return this.PRODUCT;
         }
 
-        ArrayList <Product> allProductsByChosenCategory = PRODUCT.getAllProductsBy(chosenCategory);
+        ArrayList <Product> allProductsByChosenCategory = this.PRODUCT.getAllProductsBy(chosenCategory);
         if(allProductsByChosenCategory.size() == 0){
-            view.print("No products in " + chosenCategory.getName() + " category");
-            return PRODUCT;
+            this.view.print("No products in " + chosenCategory.getName() + " category");
+            return this.PRODUCT;
         }
         
         for(int index = 0; index < allProductsByChosenCategory.size(); ++index){
@@ -183,15 +183,15 @@ public class Stock{
         }
         
         Integer productId = getNumberFromUser("Pass Product ID: ");
-        if(productId == INCORRECT_INPUT){
-            return PRODUCT;
+        if(productId == this.INCORRECT_INPUT){
+            return this.PRODUCT;
         }
         for(int index = 0; index < allProductsByChosenCategory.size(); ++index){
             if(productId.equals(allProductsByChosenCategory.get(index).getID())){
                 return allProductsByChosenCategory.get(index);
             }
         }
-        return PRODUCT;
+        return this.PRODUCT;
     }
 
     public void checkProductAvailability(){
@@ -205,9 +205,9 @@ public class Stock{
             }
         }
         if(itemCounter == 0){
-            view.print("No such product in the stock");
+            this.view.print("No such product in the stock");
         }else{
-            view.print("There are " + itemCounter + " " + productName + " in the stock.");
+            this.view.print("There are " + itemCounter + " " + productName + " in the stock.");
         }
     }  
 }
