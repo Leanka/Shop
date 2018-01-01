4340297ec85eb838d42ad0100a319dfa418786c6
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-01 03:30:17 +0100

    Stock class: Fill showAllProduct(), removeProduct(), createNewProduct()

--- frompythontojava/onlineshop/part3/Stock.java
+++ frompythontojava/onlineshop/part3/Stock.java
@@ -18,35 +18,115 @@ public class Stock{
                         "  3.Show all products",
                         "  0.Exit",
                         ""};
-        view.showList(list);
+        
+        boolean toContinue = true;
+        do{
+            view.showList(list);
+            String userChoice = view.getInput("Choose option: ");
+
+            if(userChoice.equals("0")){
+                toContinue = false;
+            }else if(userChoice.equals("1")){
+                createNewProduct();
+            }else if(userChoice.equals("2")){
+                removeProduct(); 
+            }else if(userChoice.equals("3")){
+                showAllProduct();
+            }else{
+                view.print("Incorrect choice.");
+            }
+        }while(toContinue);
     }
 
     public void categoryOptions(){
-        ;
+        String [] list = {"Category options:",
+                        "  1.Add new category",
+                        "  2.Add new featured product category",
+                        "  3.Show all categories",
+                        "  0.Exit",
+                        ""};
+        
+        boolean toContinue = true;
+        do{
+            view.showList(list);
+            String userChoice = view.getInput("Choose option: ");
+
+            if(userChoice.equals("0")){
+                toContinue = false;
+            }else if(userChoice.equals("1")){
+                addNewCategory();
+            }else if(userChoice.equals("2")){
+                addNewFeaturedCategory();
+            }else if(userChoice.equals("3")){
+                showAllCategories();
+            }else{
+                view.print("Incorrect choice.");
+            }
+        }while(toContinue);
     }
 
     public void browseOptions(){
-        ;
+        String [] list = {"Browse options:",
+                        "  1.browse all products",
+                        "  2.browse products by category",
+                        "  3.Check product availability",
+                        "  0.Exit",
+                        ""};
+        
+        boolean toContinue = true;
+        do{
+            view.showList(list);
+            String userChoice = view.getInput("Choose option: ");
+
+            if(userChoice.equals("0")){
+                toContinue = false;
+            }else if(userChoice.equals("1")){
+                getProduct();
+            }else if(userChoice.equals("2")){
+                getProductByCategory();
+            }else if(userChoice.equals("3")){
+                checkProductAvailability();
+            }else{
+                view.print("Incorrect choice.");
+            }
+        }while(toContinue);
     }
     //
-    private void addNewProduct(){
-        ;
+    private void createNewProduct(){
+        ProductCategory productCategory = getCategory();
+        String name = view.getInput("Pass new products name: ");
+        float price;
+        try{
+            price = Float.valueOf(view.getInput("Pass new products price: "));
+        }catch(NumberFormatException error_message){
+            view.print(error_message);
+            view.print("Adding product terminated.");
+            return;
+        }
+
+        new Product(name, price, productCategory);
+
     }
 
     private void removeProduct(){
-        ;
+        this.product.removeProduct(getProduct()); //zabezpieczyć przed złym wyborem/mozliwosc wycofania sie
     }
 
     private void showAllProduct(){
-        ;
+        ArrayList <Product> productList = product.getAllProducts();
+        view.print("All products:");
+        for(int index = 0; index < productList.size(); ++index){
+            view.print(productList.get(index).toString());
+        }
+        view.print();
     }
     //
     private void addNewCategory(){
-        ;
+        ; //choice feat or not
     }
 
-    private void removeCategory(){
-        ;
+    private void addNewFeaturedCategory(){
+        ; //choice feat or not
     }
 
     private void showAllCategories(){
@@ -57,8 +137,8 @@ public class Stock{
         return new Product();
     }
 
-    private Product getCategory(){
-        return new Product();
+    private ProductCategory getCategory(){
+        return new ProductCategory();
     }
 
     private Product getProductByCategory(){
