507b7a6700cdcff84740f0c6766a02faad56c16b
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-01 01:44:09 +0100

    View class: Fill getInput()

--- frompythontojava/onlineshop/part3/View.java
+++ frompythontojava/onlineshop/part3/View.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 
 public class View{
 
-    public String getInput(){
-        ;
+    public String getInput(Object message){
+        Scanner input = new Scanner(System.in);
+        String user_input;
+        
+        do{
+            print(message);
+            user_input = input.nextLine();
+        }while(user_input.trim().length() == 0);
+
+        input.close();
+        return user_input;
     }
 
     public void print(Object message){
         ;
     }
+
 }
