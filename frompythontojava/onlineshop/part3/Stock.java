4553fa4ce1b6ecd31489e3c35caaf70bfbb51280
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-01 01:32:37 +0100

    Create class Stock frame

---
+++ frompythontojava/onlineshop/part3/Stock.java
@@ -0,0 +1,66 @@
+package frompythontojava.onlineshop.part3;
+
+import java.util.ArrayList;
+
+import frompythontojava.onlineshop.part1.Product;
+import frompythontojava.onlineshop.part1.ProductCategory;
+
+public class Stock{
+    private Product product = new Product();
+    private ArrayList <ProductCategory> productCategory = new ArrayList <ProductCategory>();
+
+    //menu
+    public void productOptions(){
+        ;
+    }
+
+    public void categoryOptions(){
+        ;
+    }
+
+    public void browseOptions(){
+        ;
+    }
+    //
+    private void addNewProduct(){
+        ;
+    }
+
+    private void removeProduct(){
+        ;
+    }
+
+    private void showAllProduct(){
+        ;
+    }
+    //
+    private void addNewCategory(){
+        ;
+    }
+
+    private void removeCategory(){
+        ;
+    }
+
+    private void showAllCategories(){
+        ;
+    }
+    //
+    private Product getProduct(){
+        ;
+    }
+
+    private Product getCategory(){
+        ;
+    }
+
+    private Product getProductByCategory(){
+        ;
+    }
+
+    private void checkProductAvailability(){
+        ;
+    }
+
+    
+}
