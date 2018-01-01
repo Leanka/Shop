41e3708948a546225ffe574cd42dbfd3723abb62
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-01 22:23:16 +0100

    AbstractProcess class: Change placeholders content

--- frompythontojava/onlineshop/part3/Application.java
+++ frompythontojava/onlineshop/part3/Application.java
@@ -3,6 +3,7 @@ package frompythontojava.onlineshop.part3;
 public class Application {
 
     public static void main(String[] args) {
-	// write your code here
+        Shop shop = new Shop();
+        shop.shopOptions();
     }
 }
