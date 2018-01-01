f10d2682ab60173de1b1c7f7af65419dd23f0a46
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-01 02:13:12 +0100

    Stock class: FillproductOptions(); Add handleProductOptions()

--- frompythontojava/onlineshop/part3/Stock.java
+++ frompythontojava/onlineshop/part3/Stock.java
@@ -8,10 +8,17 @@ import frompythontojava.onlineshop.part1.ProductCategory;
 public class Stock{
     private Product product = new Product();
     private ArrayList <ProductCategory> productCategory = new ArrayList <ProductCategory>();
+    private View view = new View();
 
     //menu
     public void productOptions(){
-        ;
+        String [] list = {"Product options:",
+                        "  1.Add new product",
+                        "  2.Remove product",
+                        "  3.Show all products",
+                        "  0.Exit",
+                        ""};
+        view.showList(list);
     }
 
     public void categoryOptions(){
@@ -47,15 +54,15 @@ public class Stock{
     }
     //
     private Product getProduct(){
-        ;
+        return new Product();
     }
 
     private Product getCategory(){
-        ;
+        return new Product();
     }
 
     private Product getProductByCategory(){
-        ;
+        return new Product();
     }
 
     private void checkProductAvailability(){
