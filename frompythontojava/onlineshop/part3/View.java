4dcc27d316bf6d57264fedeb7515b7579fc0776f
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-01 01:37:47 +0100

    Create class View frame

---
+++ frompythontojava/onlineshop/part3/View.java
@@ -0,0 +1,14 @@
+package frompythontojava.onlineshop.part3;
+
+import java.util.Scanner;
+
+public class View{
+
+    public String getInput(){
+        ;
+    }
+
+    public void print(Object message){
+        ;
+    }
+}
