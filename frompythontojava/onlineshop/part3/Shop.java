f2bb669e9c4c2ad67751e2684fe7f4b9864b84fc
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-01-01 22:13:55 +0100

    Shop class: Create and fill class

---
+++ frompythontojava/onlineshop/part3/Shop.java
@@ -0,0 +1,40 @@
+package frompythontojava.onlineshop.part3;
+
+public class Shop{
+    private View view = new View();
+    private Stock stock;
+    private Client client;
+    private Staff staff;
+    
+    public Shop(){
+        this.stock = new Stock();
+        this.staff = new Staff(this.stock);
+        this.client = new Client(this.stock);
+    }
+
+    public void shopOptions(){
+        String [] list = {"Shop options:",
+                        "  1.Log in as staff member",
+                        "  2.Log in as client",
+                        "  0.Exit",
+                        ""};
+        
+        boolean toContinue = true;
+        do{
+            view.clearScreen();
+            view.showList(list);
+            String userChoice = view.getInput("Choose option: ");
+
+            if(userChoice.equals("0")){
+                toContinue = false;
+            }else if(userChoice.equals("1")){
+                this.staff.startShopping();
+            }else if(userChoice.equals("2")){
+                this.client.startShopping();
+            }else{
+                view.print("Incorrect choice.");
+                view.holdTillKeyPressed();
+            }
+        }while(toContinue);
+    }
+}
